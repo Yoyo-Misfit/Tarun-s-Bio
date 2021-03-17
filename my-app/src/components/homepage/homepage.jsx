bc82202d67bcb470854855b190105517cc1d8eb2
Tarun Alagharu <tarunalagharu@Taruns-MacBook-Pro.local>
2021-03-17 12:35:27 -0400

    Deploying First Application

---
+++ my-app/src/components/homepage/homepage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './homepage.css';
+
+class Homepage extends Component {
+
+    render() {
+        return (
+            <div className="Homepage">This is homepage</div>
+        );
+    }
+};
+
+export default Homepage;
---
+++ my-app/src/components/qac/qac.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './qac.css';
+
+
+function Qacpage() {
+    return (
+        <div className="Qacpage">
+            This is Questions and comments page
+        </div>
+    );
+}
+
+export default Qacpage;
