4b6f6c5714f0195c7d7c00f2cb55761315184b86
Tarun Alagharu <tarunalagharu@Taruns-MacBook-Pro.local>
2021-03-18 10:54:34 -0400

    Navbar component and routes

--- my-app/src/components/navbar/navbar.jsx
+++ my-app/src/components/navbar/navbar.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import './navbar.css';
+import { SocialIcon } from 'react-social-icons';
 
 class NavBar extends Component {
 
     render() {
         return (
-            <div className="navbar">
+            <div className="NavBar">
                 <a href='/about'>About</a>
                 <a href='/projects'>Projects</a>
                 <a href='/qac'>Q & C</a>
-
+                <div className="SocialLinks">
+                    <SocialIcon bgColor="black" fgColor="white" url="https://www.linkedin.com/in/tarun-alagharu-a61022187/" />
+                    <SocialIcon bgColor="black" fgColor="white" url="https://github.com/Yoyo-Misfit" />
+                    <SocialIcon bgColor="black" fgColor="white" url="mailto:tarunportfolio401@gmail.com" />
+                </div>
             </div>
 
         );
